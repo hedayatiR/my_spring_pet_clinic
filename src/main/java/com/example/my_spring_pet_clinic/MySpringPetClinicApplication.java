0d422abd6a56bc336911c75120f3b84cdf1efbd7
Reza Hedayati <gunner_1369@yahoo.com>
2019-07-17 17:22:58 +0430

    Initial Spring Boot Project Add

---
+++ src/main/java/com/example/my_spring_pet_clinic/MySpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_spring_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringPetClinicApplication.class, args);
+    }
+
+}
